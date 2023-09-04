abaa6de032cb960dfc45b1972f4e865299f28f81
Risso Leonel <risso.leonel.92@gmail.com>
2023-09-04 15:44:42 -0300

    new pages, testing proyect Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,22 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { LoginComponent } from './auth/login/login.component';
 import { RecoverComponent } from './auth/recover/recover.component';
+import { MessagesComponent } from './pages/messages/messages.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { ActivitiesComponent } from './pages/activities/activities.component';
+import { UsersComponent } from './pages/users/users.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'/inicio', pathMatch:'full'},
   {path: 'inicio', component:DashboardComponent},
   {path: 'iniciar-sesion', component:LoginComponent},
-  {path: 'recuperar', component:RecoverComponent}
+  {path: 'recuperar', component:RecoverComponent},
+  {path: 'messages', component:MessagesComponent},
+  {path: 'calendar', component:CalendarComponent},
+  {path: 'activities', component:ActivitiesComponent},
+  {path: 'users', component:UsersComponent},
+  {path: 'profile', component:ProfileComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './shared/header/header.component';
@@ -10,6 +10,14 @@ import { LoginComponent } from './auth/login/login.component';
 import { NavComponent } from './shared/nav/nav.component';
 import { RecoverComponent } from './auth/recover/recover.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { MessagesComponent } from './pages/messages/messages.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { ActivitiesComponent } from './pages/activities/activities.component';
+import { UsersComponent } from './pages/users/users.component';
+import { CalendarModule, DateAdapter } from 'angular-calendar';
+import { adapterFactory } from 'angular-calendar/date-adapters/date-fns';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -19,12 +27,20 @@ import { ReactiveFormsModule } from '@angular/forms';
     DashboardComponent,
     LoginComponent,
     NavComponent,
-    RecoverComponent
+    RecoverComponent,
+    ProfileComponent,
+    MessagesComponent,
+    CalendarComponent,
+    ActivitiesComponent,
+    UsersComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     AppRoutingModule,
     ReactiveFormsModule,
+    CalendarModule.forRoot({ provide: DateAdapter, useFactory: adapterFactory }),
+    NgbModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
